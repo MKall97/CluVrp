68d48cb1cd0e7424bf7e8ca6620faf9c7ecef5f3
MKall97 <81319007+MKall97@users.noreply.github.com>
2023-01-11 15:44:11 +0200

    Delete SaveDict.py

--- SolverPackage/SaveDict.py
+++
@@ -1,6 +0,0 @@
-def save_dict_to_json(d, filename):
-    import json
-    # Open the file for writing
-    with open(filename, "w") as f:
-        # Write the dictionary to the file as JSON
-        json.dump(d, f)
